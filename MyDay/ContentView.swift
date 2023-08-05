792f62bc382d53e408a796c9e0a53af1fc093afb
LSQzzx <790373055@qq.com>
2023-08-05 13:21:49 +0800

    Initial Commit

---
+++ MyDay/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyDay
+//
+//  Created by Sp1der on 2023/8/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyDay/MyDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDayApp.swift
+//  MyDay
+//
+//  Created by Sp1der on 2023/8/5.
+//
+
+import SwiftUI
+
+@main
+struct MyDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
